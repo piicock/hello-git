9874ae87298c9e5a051570a898d5570ef628b459
piicock <ruedahan@naver.com>
2023-11-13 10:20:37 +0900

    삭제 분기점

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
         System.out.println("hellow world");
         System.out.println("hellow git");
-        System.out.println("손시렵다");
 	}
 
 }
