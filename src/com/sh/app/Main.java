acec9ea3836fb753dd625f0ef63d61a5be56ab31
piicock <ruedahan@naver.com>
2023-11-13 10:22:34 +0900

    Revert "수정 분기범"
    This reverts commit 74790ec1f55ecc77d12ffb94ccd037bf5dec9341.

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
         System.out.println("hellow world");
-        System.out.println("hellow gggggit");
+        System.out.println("hellow git");
 	}
 
 }
