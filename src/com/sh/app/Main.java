56e7eb9c40dadc38608265a3f5772fc95264b2d3
piicock <ruedahan@naver.com>
2023-11-13 10:20:15 +0900

    손시려워

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
         System.out.println("hellow world");
         System.out.println("hellow git");
+        System.out.println("손시렵다");
 	}
 
 }
