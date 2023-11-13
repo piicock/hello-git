6302d4f3b0326e77a6be10071b7244f4b8faf7c7
piicock <ruedahan@naver.com>
2023-11-13 10:33:54 +0900

    new Dog#brak 추가

---
+++ src/com/sh/animal/Dog.java
@@ -0,0 +1,8 @@
+package com.sh.animal;
+
+public class Dog {
+    public void bark() {
+      System.out.println("멍멍");
+    	
+	}
+}
--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -1,10 +1,16 @@
 package com.sh.app;
 
+import com.sh.animal.Dog;
+
 public class Main {
 
 	public static void main(String[] args) {
         System.out.println("hellow world");
         System.out.println("hellow git");
+	
+	    new Dog().bark();
+	    
+
 	}
 
 }
