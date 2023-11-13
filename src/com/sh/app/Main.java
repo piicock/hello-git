665a7ccb948dba9d3e0abc209fc5c8377253f1a5
piicock <ruedahan@naver.com>
2023-11-13 11:22:10 +0900

    Main.main 대문자로 수정

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -7,12 +7,15 @@ import com.sh.app.animal.Pig;
 public class Main {
 
 	public static void main(String[] args) {
-        System.out.println("hellow world");
-        System.out.println("hellow git");
-	
-	    new Dog().bark();
+		//컨 쉬 X : 대문자로 변경해줌
+        System.out.println("HELLOW WORLD");
+        System.out.println("HELLOW GIT");
+
+        new Dog().bark();
 	    new Cat().jump();
 	    new Pig().meat();
+	    
+	   
 	    
 	}
 
--- src/com/sh/app/animal/Pig.java
+++ src/com/sh/app/animal/Pig.java
@@ -2,6 +2,6 @@ package com.sh.app.animal;
 
 public class Pig {
       public void meat() {
-       System.out.println("돼지는 맛있는 고기가 되었습니다");
+       System.out.println("돼지 고기가 되었습니다");
 	}
 }
